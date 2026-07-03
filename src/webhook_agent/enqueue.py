334f123f5d34a64f8d6344e6b7b48e521218c5b1
cgj8702-agents <299140917+cgj8702-agents@users.noreply.github.com>
2026-07-02 21:25:44 -0400

    shorten log messages to my preference

--- src/webhook_agent/enqueue.py
+++ src/webhook_agent/enqueue.py
@@ -32,7 +32,7 @@ def publish_webhook_message(
         def _cb(fut):
             try:
                 message_id = fut.result()
-                logger.info("📨 Enqueued delivery: %s", message_id)
+                logger.info("📨 Enqueued delivery: %s", str(message_id)[-4:])
             except Exception:
                 logger.exception("publish failed to %s", topic_path)
 
